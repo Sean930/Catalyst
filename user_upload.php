a6c9be8368ef4767a20cc218005ff0d1c2a7915d
Sean930 <Sean@192.168.0.4>
2016-02-12 12:39:09 +1100

    Bugs fixing, adding comments.

--- user_upload.php
+++ user_upload.php
@@ -1,4 +1,6 @@
 <?php
+error_reporting ( 0 );
+// Global variables
 $users = array ();
 $usr;
 $pwd;
@@ -39,22 +41,25 @@ do {
 			echo "Undefined command, please enter again.\n";
 	}
 } while ( $input != "e" );
+
+// Connection to database
 function connection() {
 	global $usr, $pwd, $host, $con;
-	// if ($usr != null && $host != null) {
-	// $con = mysqli_connect ( $host, $usr, $pwd );
-	if (1) {
-		$con = mysqli_connect ( "localhost:3306", "root", "root" );
+	if ($usr != null && $host != null) {
+		$con = mysqli_connect ( $host, $usr, $pwd );
 		if (! $con) {
-			fwrite ( STDOUT, "Failed to connect to MySql: " . mysql_error () );
+			fwrite ( STDOUT, "Failed to connect to MySql: " . mysqli_connect_error () . "\n" );
 			return false;
 		}
-		echo "Successfully connected.\n";
+		echo "Successfully connected to database.\n";
 		return true;
-	}
-	else 
+	} else {
+		echo "Please enter MySQL username, password and host address first.\n";
 		return false;
+	}
 }
+
+// Parse file and store data in a global array. Assuming csv file is located in the same directory of this file.
 function parseData() {
 	fwrite ( STDOUT, "Enter file name: " );
 	
@@ -77,14 +82,16 @@ function parseData() {
 		fwrite ( STDOUT, "File successfully parsed.\n" );
 	}
 }
+
+// Insert data into MySQL, check if table 'users' exists first, if it does not exist, print error and ask user to create the table first.
 function insertData() {
-	if (connection ()) {
+	if (! connection ()) {
+		echo "Cannot connect to MySQL.\n";
+	} else {
 		global $con, $users;
-		$sql = "SELECT name FROM users";
+		$sql = "SELECT TABLE_NAME FROM `INFORMATION_SCHEMA`.`TABLES` WHERE TABLE_NAME ='users' AND TABLE_SCHEMA='my_db';";
 		$result = mysqli_query ( $con, $sql );
-		print_r ( $result );
-		echo mysqli_num_rows ( $result );
-		if ($result == null) {
+		if (mysqli_num_rows ( $result ) != null) {
 			for($i = 1; $i < count ( $users ); $i ++) {
 				$users [$i] [2] = trim ( $users [$i] [2] );
 				if (filter_var ( $users [$i] [2], FILTER_VALIDATE_EMAIL )) {
@@ -97,11 +104,14 @@ function insertData() {
 					fwrite ( STDOUT, $users [$i] [2] . " is not a valid email address.\n" );
 				}
 			}
+			mysqli_close ( $con );
 		} else {
 			fwrite ( STDOUT, "Table 'users' is not exist, please create table first.\n" );
 		}
 	}
 }
+
+// Create table users in MySQL. If there is no database named my_db, create it first.
 function createTable() {
 	if (connection ()) {
 		global $con;
@@ -120,8 +130,7 @@ function createTable() {
 		}
 		
 		mysqli_close ( $con );
-	} else
-		fwrite ( STDOUT, "Please enter MySql username, password and host address first!\n" );
+	}
 }
 function MySQLUsername() {
 	fwrite ( STDOUT, "Please enter MySql username: " );
@@ -139,12 +148,14 @@ function MySQLHost() {
 	$host = trim ( fgets ( STDIN ) );
 }
 function help() {
-	echo "-- file: this is the name of the CSV to be parsed\n";
+	echo "-- file: parse csv file and store data into database.\n";
 	echo "--create_table: this will cause the MySQL users table to be built (and no further action will be taken)\n";
 	echo "--dry_run: this will be used with the -- file directive in the instance that we want to run the script but not insert into the DB. All other functions will be executed, but the database won't be altered.\n";
 	echo "-u: MySQL username\n";
 	echo "-p: MySQL password\n";
 	echo "-h: MySQL host\n";
+	echo "--help: directives help information\n";
+	echo "-e: quit\n";
 }
 
 ?>
